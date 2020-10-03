e5d7dd6dad9b539a5b5324a3dc59383c634af447
Thomas Sherer <tcpma2011@gmail.com>
2020-10-03 14:05:46 -0700

    Began customizing App.js to this lab.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tools: [
+        {
+          id: 1,
+          name : 'Screwdriver',
+          type: 'driver',
+        },
+        {
+          id: 2,
+          name : 'Hammer',
+          type : 'driver',
+        },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div className='App'>
+          <Header tools={this.state.tools} />
+          <Footer />
+        </div>
+      </>
+    )
+  }
+}
+
+
+function Header(props) {
+  return (
+    <>
+      <h1>Tool List</h1>
+  <h2>Number of tools in current list: {props.tools.length}</h2>
+    </>
+  )
+}
+
+function Footer(props) {
+  return (
+    <h4>&#169; 2020, Chuck & Tom Creations</h4>
+  )
+}
 
 export default App;
